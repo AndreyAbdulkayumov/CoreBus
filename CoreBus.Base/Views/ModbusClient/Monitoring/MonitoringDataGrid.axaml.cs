5e08d71c88d227b705bc4404363168254c85a777
AndreyAbdulkayumov <86914394+AndreyAbdulkayumov@users.noreply.github.com>
2025-12-06 20:27:38 +0300

    Переместил таблицу в отдельный контрол.

---
+++ CoreBus.Base/Views/ModbusClient/Monitoring/MonitoringDataGrid.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CoreBus.Base.Views.ModbusClient.Monitoring;
+
+public partial class MonitoringDataGrid : UserControl
+{
+    public MonitoringDataGrid()
+    {
+        InitializeComponent();
+    }
+}
