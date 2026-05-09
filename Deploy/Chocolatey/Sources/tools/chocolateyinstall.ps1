32a0d149a19a87cf6070d14a18b1c9ba3e2f7084
Андрей Абдулкаюмов <86914394+AndreyAbdulkayumov@users.noreply.github.com>
2026-05-09 15:04:01 +0300

    Обновил деплой на Chocolatey.

--- Deploy/Chocolatey/Sources/tools/chocolateyinstall.ps1
+++ Deploy/Chocolatey/Sources/tools/chocolateyinstall.ps1
@@ -4,9 +4,9 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = $env:ChocolateyPackageName
 $softwareName = 'CoreBus*'
 
-$fileLocation = Join-Path $toolsDir 'CoreBus_3.4.0_win_x64_installer.exe'
+$fileLocation = Join-Path $toolsDir 'CoreBus_3.5.0_win_x64_installer.exe'
 
-$checksum = 'c9886cab4e64a70aad52c04b11b3f190ca2b9e660cab03822dacf8fff6d70495'
+$checksum = 'c3c47175ebc51cfcdcaad2fa439ce7001742639d5183bc2f10c9eb5fcf324de6'
 
 $packageArgs = @{
   packageName    = $packageName
